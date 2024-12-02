2e8d04897eb6ab7fa8d6bfec6cf4a7ff80a619b2
bchitadze <beqachita@gmail.com>
2024-12-02 22:36:39 +0400

    davamate davaleba 2-is WaitsTest da WebFormsTest.

---
+++ src/test/java/WaitsTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class WaitsTest {
+    @Test
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/progress-bar");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.findElement(By.xpath("//*[@id=\"startStopButton\"]")).click();
+        WebElement message = driver.findElement(By.xpath("//*[@id=\"progressBar\"]/div"));
+        System.out.println("შედეგი: " + message.getText() + " 100%");
+
+
+
+    }
+}
---
+++ src/test/java/WebFormsTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class WebFormsTest {
+    @Test
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+
+        driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-1\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-1\"]/option[2]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-2\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-2\"]/option[3]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-3\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"dropdowm-menu-3\"]/option[3]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/label[1]/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/label[2]/input")).click();
+        driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/label[4]/input")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"radio-buttons\"]/input[3]")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"fruit-selects\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"fruit-selects\"]/option[2]")).click();
+
+        WebElement value = driver.findElement(By.xpath("//*[@id=\"fruit-selects\"]/option[2]"));
+        System.out.println(value.getText() + " disabled" );
+
+        driver.close();
+    }
+}
