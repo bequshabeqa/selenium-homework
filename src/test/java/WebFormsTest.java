7240be6b1fa176bf5360813e45e80c18e5344168
bchitadze <beqachita@gmail.com>
2024-12-02 22:37:35 +0400

    davamate davaleba 2-is WaitsTest da WebFormsTest.

--- src/test/java/WebFormsTest.java
+++ src/test/java/WebFormsTest.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class WebFormsTest {
     @Test
     public static void main(String[] args) {
